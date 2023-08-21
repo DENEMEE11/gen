77d25c10102e7b0e13b1c103b6c3a7ed77f795ae
DENEMEE11 <97140083+DENEMEE11@users.noreply.github.com>
2023-08-21 06:37:29 +0300

    Update gen.py

--- gen.py
+++ gen.py
@@ -4,39 +4,52 @@ from datetime import datetime
 from threading import Thread
 import os
 import sys
-from concurrent.futures import ThreadPoolExecutor
+import psutil
 
 def check_and_send(wif, master_address):
     try:
         wallet = Key(wif)
-        balance_btc = wallet.get_balance('btc')
-        if float(balance_btc) >= 0:
-            print(f"[!] > Checking {wallet.address} | Balance: {balance_btc}")
+        if float(wallet.get_balance('btc')) >= 0:
+            print(f"[!] > Checking {wallet.address} | Balance: {wallet.get_balance('btc')}")
             tx = wallet.send([], leftover=master_address, unspents=wallet.get_unspents())
-            print(f"[!] ({datetime.now().replace(microsecond=0)}) > Balance found | {balance_btc} | {wallet.address}\n\nTransaction Hash: {tx}")
+            print(f"[!] ({datetime.now().replace(microsecond=0)}) > Balance found | {wallet.get_balance()} | {wallet.address}\n\nTransaction Hash: {tx}")
             with open("found.txt", "a") as found_file:
-                found_file.write(f"{datetime.now().replace(microsecond=0)} | {wallet.segwit_address} | Balance: {balance_btc}\n")
+                found_file.write(f"{datetime.now().replace(microsecond=0)} | {wallet.segwit_address} | Balance: {wallet.get_balance()}")
         else:
             pass
-    except (ValueError, KeyboardInterrupt):
+    except ValueError:
         pass
-    except Exception as e:
-        print("Error:", e)
-
+    except KeyboardInterrupt:
+        sys.exit("[!] > Goodbye!")
+    except Exception:
+        raise Exception
+    
 def generate(amount, thread_number, master_address):
     for i in range(0, amount):
+        pass
         wallet = Wallet()
+        pass
         check_and_send(wallet.__dict__['key'].__dict__['mainnet'].__dict__['wif'], master_address)
+    return 
+    
+def set_cpu_affinity():
+    pid = os.getpid()
+    p = psutil.Process(pid)
+    p.cpu_affinity([0])  # Limit to the first CPU core
 
-def main():
-    os.system("clear")
-    print("BTC Checker - Bitcoin address bruteforcer.")
-    print("Amount of addresses to generate equals to Amount of addresses on each thread * Number of threads.")
+if __name__ == "__main__":
+    os.system("cls" if os.name == "nt" else "clear")
+    print("""
+    # ... (your ASCII art and other print statements)
+    """)
+    print("[!] > BTC Checker - Bitcoin address bruteforcer.")
+    print("[!] > Amount of addresses to generate equals to Amount of addresses on each thread * Number of threads.")
     master_address = input("[?] > Address to send funds to: ")
     amount = int(input("[?] > How many addresses would you like to generate on each thread?: "))
     threads = int(input("[?] > How many threads would you like to run?: "))
+    
+    # Start threads with limited CPU affinity
     for i in range(0, threads):
-        thread = Thread(target=generate, args=(amount, i, master_address)).start()
-
-if __name__ == "__main__":
-    main()
+        thread = Thread(target=generate, args=(amount, i, master_address))
+        thread.start()
+        set_cpu_affinity()
